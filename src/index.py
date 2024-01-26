afb05f16bd8795d47a59732590085492075d958f
Rei-69 <hasaan6691@gmail.com>
2024-01-26 18:30:09 +0500

    modified:   src/index.py

--- src/index.py
+++ src/index.py
@@ -8,17 +8,17 @@ client = commands.Bot(command_prefix='!', intents=intents)
 
 load_dotenv()
 
-# Get the Discord bot token from the environment variables
+# Discord bot token
 TOKEN = os.getenv('TOKEN')
 
 @client.event
 async def on_ready():
     print('Bot is ready!')
 
-    # Set streaming status
+    # Streaming status
     await client.change_presence(activity=discord.Streaming(name='Cooking', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
 
-# Add your other event handlers and bot functionality here
+# Event handlers and bot functionality here
 @client.event
 async def on_message(message):
     if message.author == client.user:
